7e0bb090a99737f97a035780ffd2e79263a24c62
Saurav Mishra <sauravmishra7681@gmail.com>
2026-01-20 04:36:42 +0000

    Try fixing this error: `Build Error: Export FirebaseClientProvider doesn

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -22,6 +22,7 @@ export function initializeFirebase(): {
 }
 
 export * from './provider';
+export * from './client-provider';
 export * from './auth/use-user';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
